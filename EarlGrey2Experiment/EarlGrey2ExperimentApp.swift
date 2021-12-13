0ae6508e99220e12d0263ffb2c4a6d3a7a6d3bf2
Adil Hussain <adilson05uk@gmail.com>
2021-12-13 12:28:44 +0000

    Converted the `EarlGrey2Experiment` target from a UIKit application to a SwiftUI application.

---
+++ EarlGrey2Experiment/EarlGrey2ExperimentApp.swift
@@ -0,0 +1,18 @@
+//
+//  EarlGrey2SwiftUIExperimentApp.swift
+//  EarlGrey2SwiftUIExperiment
+//
+//  Created by Adil Hussain on 13/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct EarlGrey2ExperimentApp: App {
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
