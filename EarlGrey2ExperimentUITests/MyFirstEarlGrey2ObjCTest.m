2d0c80bba163a86e369c06ef13386c20203648ed
Adil Hussain <adilson05uk@gmail.com>
2021-12-13 13:00:44 +0000

    Added a couple of tests in `MyFirstEarlGrey2ObjCTest`.

--- EarlGrey2ExperimentUITests/MyFirstEarlGrey2ObjCTest.m
+++ EarlGrey2ExperimentUITests/MyFirstEarlGrey2ObjCTest.m
@@ -5,7 +5,7 @@
 //  Created by Adil Hussain on 09/12/2021.
 //
 
-@import XCTest;
+#import <XCTest/XCTest.h>
 
 #import "TestLib/EarlGreyImpl/EarlGrey.h"
 
@@ -14,13 +14,21 @@
 
 @implementation MyFirstEarlGrey2ObjCTest
 
-- (void)testIfKeyWindowIsPresent {
-    // XCUITest Setup
+- (void)setUp {
     XCUIApplication *application = [[XCUIApplication alloc] init];
     [application launch];
-    
-    // EarlGrey Statement
+}
+
+- (void)test_key_window_is_present {
     [[EarlGrey selectElementWithMatcher:grey_keyWindow()] performAction:grey_tap()];
+}
+
+- (void)test_hello_world_label_is_visible {
+    [[EarlGrey selectElementWithMatcher:grey_accessibilityID(@"HelloWorldLabel")] assertWithMatcher:grey_sufficientlyVisible()];
+}
+
+- (void)test_hello_world_label_has_expected_text {
+    [[EarlGrey selectElementWithMatcher:grey_accessibilityID(@"HelloWorldLabel")] assertWithMatcher:grey_text(@"Hello, world!")];
 }
 
 @end
